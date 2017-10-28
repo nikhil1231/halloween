e4ed9282fc8a5611b5cdd9d4b7ada5f1fb147efd
nikhil1231 <nikhil1231@hotmail.co.uk>
2017-10-28 16:38:53 +0100

    postcode button

--- js/init.js
+++ js/init.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	var postcode = "se21 8ae";
 
+	$('#postcode-input').click(function(){
+		
+	})
+
 	$.ajax("http://api.postcodes.io/postcodes/" + postcode.replace(/\s/g,''),{
 		success: function(data){
 			console.log(data);
@@ -10,3 +14,9 @@ $(document).ready(function(){
 		}
 	})
 })
+
+function valid_postcode(postcode) {
+    postcode = postcode.replace(/\s/g, "");
+    var regex = /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})$/i;
+    return regex.test(postcode);
+}
