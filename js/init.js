cf23d09470a07a2d7fbdf4945516353477af5fce
nikhil1231 <nikhil1231@hotmail.co.uk>
2017-10-28 15:36:24 +0100

    initial commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var postcode = "se21 8ae";
+
+	$.ajax("http://api.postcodes.io/postcodes/" + postcode.replace(/\s/g,''),{
+		success: function(data){
+			console.log(data);
+		},
+		error: function(err){
+			console.log(err);
+		}
+	})
+})
