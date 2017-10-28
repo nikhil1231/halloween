cbb4fe314e996ac12468427eb2bb413dd69f2851
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-10-28 10:22:27 -0500

    added json file

---
+++ js/jsonRequest.js
@@ -0,0 +1,36 @@
+//API https://data.police.uk/docs/method/crime-street/
+
+var request = new XMLHttpRequest();
+var date = new Date();
+var previousDate = new Date(date.setMonth(date.getMonth() - 1));
+var month = previousDate.getMonth();
+var year = previousDate.getFullYear();
+
+if (month < 10){
+    month = ("0" + month);
+}
+
+console.log(previousDate);
+console.log(month + " " + year);
+
+var userlat = "52.627161";
+var userlng = "-1.131592";
+
+var requestURL = "https://data.police.uk/api/crimes-street/all-crime?lat=" + userlat + "&lng=" + userlng + "&date=" + year + "-" + month;
+
+console.log(requestURL);
+
+request.open('GET', requestURL, true);
+request.responseType = 'json';
+request.send();
+
+function returnLocation(){
+    "use strict";
+    var fullLocations = request.response;
+    
+    console.log(fullLocations);
+    
+    
+}
+
+returnLocation();
