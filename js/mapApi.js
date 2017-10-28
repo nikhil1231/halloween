c155c775b22c2c5da5e9e9b0029ade185372c6a9
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-10-28 12:37:36 -0500

    changes

--- js/mapApi.js
+++ js/mapApi.js
@@ -2,11 +2,8 @@ function initMap(locations, userlat, userlng) {
     if (locations === undefined){
         return null;
     }
-    console.trace();
-    console.log("here initMap " + locations.length);
     var center = new google.maps.LatLng(userlat, userlng);
 
-    
     var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 3,
           center: center,
@@ -18,8 +15,11 @@ function initMap(locations, userlat, userlng) {
     for (var i = 0; i < locations.length; i++){
         var latLng = new google.maps.LatLng(locations[i].lat,
       locations[i].lng);
+        
+        console.log(latLng);
         var marker = new google.maps.Marker({'position': latLng});
         markers.push(marker);
+
     }
    
     var markerCluster = new MarkerClusterer(map, markers);
